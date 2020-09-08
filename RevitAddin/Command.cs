52f77ba1f946641103d42a25424b0af810ce1685
Edier García Gutierrez <ediergg@gmail.com>
2020-09-08 18:44:59 -0400

    Adding the Windows Form to obtain the necessary data for the creation of the building

--- RevitAddin/Command.cs
+++ RevitAddin/Command.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Windows.Forms;
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
@@ -14,44 +15,31 @@ namespace RevitAddin
     [Transaction(TransactionMode.Manual)]
     public class Command : IExternalCommand
     {
-        public Result Execute(
-          ExternalCommandData commandData,
-          ref string message,
-          ElementSet elements)
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            UIApplication uiapp = commandData.Application;
-            UIDocument uidoc = uiapp.ActiveUIDocument;
-            Application app = uiapp.Application;
-            Document doc = uidoc.Document;
-
-            // Access current selection
-
-            Selection sel = uidoc.Selection;
-
-            // Retrieve elements from database
-
-            FilteredElementCollector col
-              = new FilteredElementCollector(doc)
-                .WhereElementIsNotElementType()
-                .OfCategory(BuiltInCategory.INVALID)
-                .OfClass(typeof(Wall));
-
-            // Filtered element collector is iterable
-
-            foreach (Element e in col)
+            try
             {
-                Debug.Print(e.Name);
+                var buildingManager = new BuildingManager(commandData);
+
+                DialogResult result = DialogResult.None;
+                using (frmBuildingCreationData form = new frmBuildingCreationData(buildingManager))
+                {
+                    result = form.ShowDialog();
+                }
+
+                if (result == DialogResult.OK)
+                {
+                    return Result.Succeeded;
+                }
+
+                return Result.Cancelled;
             }
-
-            // Modify document within a transaction
-
-            using (Transaction tx = new Transaction(doc))
+            catch (Exception ex)
             {
-                tx.Start("Transaction Name");
-                tx.Commit();
-            }
+                message = ex.Message;
 
-            return Result.Succeeded;
+                return Result.Failed;
+            }
         }
     }
 }
---
+++ RevitAddin/frmBuildingCreationData.cs
@@ -0,0 +1,49 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RevitAddin
+{
+    public partial class frmBuildingCreationData : Form
+    {
+        private readonly BuildingManager buildingManager;
+
+        public frmBuildingCreationData()
+        {
+            InitializeComponent();
+        }
+
+        public frmBuildingCreationData(BuildingManager buildingManager): this()
+        {
+            this.buildingManager = buildingManager;
+        }
+
+        private void frmBuildingCreationData_Load(object sender, EventArgs e)
+        {
+            this.cbRoofType.DataSource = buildingManager.RoofTypes;
+            this.cbRoofType.DisplayMember = "Name";
+            this.cbRoofType.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            this.cbWallType.DataSource = buildingManager.WallTypes;
+            this.cbWallType.DisplayMember = "Name";
+            this.cbWallType.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void chbRoofType_CheckedChanged(object sender, EventArgs e)
+        {
+            cbRoofType.Enabled = chbRoofType.Checked;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            TaskDialog.Show("Building creation", "Hello world!");
+        }
+    }
+}
