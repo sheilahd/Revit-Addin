8e03e2c3702d8009aab78a2fb479ba45b1e9a44a
Edier García Gutierrez <ediergg@gmail.com>
2020-09-16 13:47:09 -0400

    Adding chm file for the contextual help of the commands.

--- Hazen/App.cs
+++ Hazen/App.cs
@@ -21,16 +21,19 @@ namespace Hazen
             // Create buttons
             string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
             string imagePath = Path.Combine(Path.GetDirectoryName(thisAssemblyPath), "Images");
+            string helpPath = Path.GetDirectoryName(thisAssemblyPath);
 
             PushButtonData button1 = new PushButtonData("btnNewProj", "New Project", thisAssemblyPath, typeof(cmdNewProj).FullName);
             PushButton pushButton1 = hazenPanel.AddItem(button1) as PushButton;
             pushButton1.LargeImage = new BitmapImage(new Uri(Path.Combine(imagePath, "HazenIcon96x96.png")));
             pushButton1.ToolTip = "Start a new project by collecting data from the user. Once you have filled out the form, a new 3D model will automatically be created";
+            pushButton1.SetContextualHelp(new ContextualHelp(ContextualHelpType.ChmFile, Path.Combine(helpPath, "Hazen.chm")));
 
             PushButtonData button2 = new PushButtonData("btnNewSheet", "New Sheet", thisAssemblyPath, typeof(cmdNewSheet).FullName);
             PushButton pushButton2 = hazenPanel.AddItem(button2) as PushButton;
             pushButton2.LargeImage = new BitmapImage(new Uri(Path.Combine(imagePath, "NewSheetIcon96x96.png")));
             pushButton2.ToolTip = "Create a new sheet by collecting data from the user. Once you have filled out the form, a new populated sheet will automatically be created";
+            pushButton2.SetContextualHelp(new ContextualHelp(ContextualHelpType.ChmFile, Path.Combine(helpPath, "Hazen.chm")));
 
             return Result.Succeeded;
         }
