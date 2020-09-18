a3ca84fabdcc4afcc8cf7edcd2482fa20948949c
Edier García Gutierrez <ediergg@gmail.com>
2020-09-18 17:55:39 -0400

    Adding missing classes

---
+++ Hazen/Utils.cs
@@ -0,0 +1,77 @@
+﻿using Autodesk.Revit.DB;
+using System;
+
+namespace Hazen
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="levelBottom"></param>
+        /// <param name="levelTop"></param>
+        /// <returns></returns>
+        public static bool GetBottomAndTopLevels(Document doc, ref Level levelBottom, ref Level levelTop)
+        {
+            FilteredElementCollector levels = GetElementsOfType(doc, typeof(Level), BuiltInCategory.OST_Levels);
+
+            foreach (Element e in levels)
+            {
+                if (null == levelBottom)
+                {
+                    levelBottom = e as Level;
+                }
+                else if (null == levelTop)
+                {
+                    levelTop = e as Level;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            bool bothLevels = null != levelBottom && null != levelTop;
+
+            if (bothLevels && levelTop.Elevation < levelBottom.Elevation)
+            {
+                Level tmp = levelTop;
+                levelTop = levelBottom;
+                levelBottom = tmp;
+            }
+
+            return bothLevels;
+        }
+
+        /// <summary>
+        /// Return all elements of the requested class,
+        /// i.e. System.Type, matching the given built-in
+        /// category in the given document.
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="type"></param>
+        /// <param name="bic"></param>
+        /// <returns></returns>
+        public static FilteredElementCollector GetElementsOfType(Document doc, Type type, BuiltInCategory bic)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+
+            collector.OfCategory(bic);
+            collector.OfClass(type);
+
+            return collector;
+        }
+
+        /// <summary>
+        /// Return the midpoint between two points.
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public static XYZ Midpoint(XYZ p, XYZ q)
+        {
+            return p + 0.5 * (q - p);
+        }
+    }
+}
