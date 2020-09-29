841f1cccc05faa3ac4c26b3d3f44bf822d0ccf74
edelkish <edelkish@EDELKIS-PC>
2020-09-29 09:26:47 -0400

    Ordenadas alfabéticamente de forma ascendente las listas de walls and roof presentes en el formulario

--- Hazen/Managers/NewProjManager.cs
+++ Hazen/Managers/NewProjManager.cs
@@ -30,11 +30,11 @@ namespace Hazen.Managers
 
             // Search all the roof types in the Revit
             FilteredElementCollector roofTypesElementCollector = Utils.GetElementsOfType(doc, typeof(RoofType), BuiltInCategory.OST_Roofs);
-            m_roofTypes = roofTypesElementCollector.Cast<RoofType>().ToList();
+            m_roofTypes = roofTypesElementCollector.Cast<RoofType>().OrderBy(rt => rt.FamilyName).ToList();
 
             // Search all the wall types in the Revit
             FilteredElementCollector wallTypesElementCollector = Utils.GetElementsOfType(doc, typeof(WallType), BuiltInCategory.OST_Walls);
-            m_wallTypes = wallTypesElementCollector.Cast<WallType>().ToList();
+            m_wallTypes = wallTypesElementCollector.Cast<WallType>().OrderBy(wt => wt.FamilyName).ToList();
         }
 
         public ReadOnlyCollection<RoofType> RoofTypes
