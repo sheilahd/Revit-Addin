e377349a75e77b09d5b39ed6777378a77fa8c082
Lester Molina <lecartuchen@gmail.com>
2020-10-29 00:38:29 -0400

    Replaced Icon imagen for 1st button and changed button name.

--- Beva/App.cs
+++ Beva/App.cs
@@ -42,9 +42,9 @@ namespace Beva
             string imagePath = Path.Combine(Path.GetDirectoryName(thisAssemblyPath), "Images");
             string helpPath = Path.GetDirectoryName(thisAssemblyPath);
 
-            PushButtonData button1 = new PushButtonData("btnNewProj", "New Project", thisAssemblyPath, typeof(cmdNewProj).FullName);
+            PushButtonData button1 = new PushButtonData("btnNewProj", "EZ-Build", thisAssemblyPath, typeof(cmdNewProj).FullName);
             PushButton pushButton1 = panel.AddItem(button1) as PushButton;
-            pushButton1.LargeImage = new BitmapImage(new Uri(Path.Combine(imagePath, "NewProjectIcon96x96.png")));
+            pushButton1.LargeImage = new BitmapImage(new Uri(Path.Combine(imagePath, "Xpress-Bldg_Btn32x32.png")));
             pushButton1.ToolTip = "Start a new project by collecting data from the user. Once you have filled out the form, a new 3D model will automatically be created";
             pushButton1.SetContextualHelp(new ContextualHelp(ContextualHelpType.ChmFile, Path.Combine(helpPath, "Beva.chm")));
 
@@ -52,7 +52,7 @@ namespace Beva
 
             PushButtonData button2 = new PushButtonData("btnNewSheet", "New Sheet", thisAssemblyPath, typeof(cmdNewSheet).FullName);
             PushButton pushButton2 = panel.AddItem(button2) as PushButton;
-            pushButton2.LargeImage = new BitmapImage(new Uri(Path.Combine(imagePath, "NewSheetIcon96x96.png")));
+            pushButton2.LargeImage = new BitmapImage(new Uri(Path.Combine(imagePath, "Xpress-Bldg_Btn32x32.png")));
             pushButton2.ToolTip = "Create a new sheet by collecting data from the user. Once you have filled out the form, a new populated sheet will automatically be created";
             pushButton2.SetContextualHelp(new ContextualHelp(ContextualHelpType.ChmFile, Path.Combine(helpPath, "Beva.chm")));
 
