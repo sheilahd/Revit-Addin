3a4bcbfa326cb39eed34b1ede392289747399921
edelkish <edelkishh25@gmail.com>
2020-11-15 12:10:40 -0500

    Añadido a la clase NewSheetData los datos a recoger desde el formulario.
    Issues #42

--- Beva/FormData/NewSheetData.cs
+++ Beva/FormData/NewSheetData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autodesk.Revit.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,46 @@ using System.Threading.Tasks;
 
 namespace Beva.FormData
 {
-    class NewSheetData
+    public class NewSheetData
     {
+        public View FloorViewTemplate { get; set; }
+
+        public View RoofViewTemplate { get; set; }
+
+        public View NorthElevationViewTemplate { get; set; }
+
+        public View SouthElevationViewTemplate { get; set; }
+
+        public View WestElevationViewTemplate { get; set; }
+
+        public View EastElevationViewTemplate { get; set; }
+
+        public bool SelectRoofViewTemplate { get; set; }
+
+        public bool SelectFloorViewTemplate { get; set; }
+
+        public bool SelectNorthElevationViewTemplate { get; set; }
+
+        public bool SelectSouthElevationViewTemplate { get; set; }
+
+        public bool SelectWestElevationViewTemplate { get; set; }
+
+        public bool SelectEastElevationViewTemplate { get; set; }
+
+        public View TitleBlockViewTemplate { get; set; }
+
+        public bool SelectTitleBlockViewTemplate { get; set; }
+
+        public string ProjectName { get; set; }
+
+        public string ProjectNumber { get; set; }
+
+        public string Discipline { get; set; }
+
+        public string DrawnBy { get; set; }
+
+        public string CheckedBy { get; set; }
+
+        public string ApprovedBy { get; set; }
     }
 }
